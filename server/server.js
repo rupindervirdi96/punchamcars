bc51240f6ad4ab7f56ae0169a8f26712b08cd3af
Rupinder Virdi <rupindervirdi96@gmail.com>
2026-01-18 22:11:12 -0500

    server fix

--- server/server.js
+++ server/server.js
@@ -9,9 +9,9 @@ const server = require("http").createServer(app);
 const connectDB = require('./config/db');
 var cors = require('cors');
 
+connectDB();
 const port = process.env.PORT || 5000;
 
-connectDB();
 
 app.use(express.json({ extended: false }));
 app.use(cors());
@@ -20,12 +20,13 @@ app.use(cors());
 app.use('/api', require('./routes/app.routes'));
 
 // Serve React build as static files
-app.use(express.static(path.join(__dirname, '..', 'build')));
-
-// For any non-API route, send back React's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.setHeader("Access-Control-Allow-Credentials","true")
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

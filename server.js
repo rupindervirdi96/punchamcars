58cf677ef80049d2e007e22282a9a77876158d1f
Rupinder Virdi <rupindervirdi96@gmail.com>
2026-01-20 15:53:40 -0500

    server fix

--- server.js
+++ server.js
@@ -15,11 +15,8 @@ app.use(
 
 app.use("/api", require("./routes/app.routes.js"));
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+app.use(express.static(path.join(__dirname, "client/build"))); 
+app.get("*", (req, res) => { res.sendFile(path.join(__dirname, "client/build", "index.html")); });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
